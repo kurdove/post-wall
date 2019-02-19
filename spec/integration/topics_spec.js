7c42e4fb33fa169e96128728c56f163f01db1d30
Evgheni Curdov <kurdov.e@gmail.com>
2019-02-18 21:26:14 -0800

    coding challenge progrss

---
+++ spec/integration/topics_spec.js
@@ -0,0 +1,84 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/topics/";
+const sequelize = require("../../src/db/models/index").sequelize;
+const Topic = require("../../src/db/models").Topic;
+
+
+describe("routes : topics", () => {
+    beforeEach((done) => {
+        this.topic;
+        sequelize.sync({force: true}).then((res) => {
+  
+         Topic.create({
+           title: "JS Frameworks",
+           description: "There is a lot of them"
+         })
+          .then((topic) => {
+            this.topic = topic;
+            done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+  
+        });
+  
+    });
+
+    describe("GET /topics", () => {
+
+        it("should return a status code 200 and all topics", (done) => {
+            request.get(base, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                expect(err).toBeNull();
+                expect(body).toContain("Topics");
+                expect(body).toContain("JS Frameworks");
+                done();
+            });
+        });
+        
+    });
+
+    describe("GET /topics/new", () => {
+
+        it("should render a new topic form", (done) => {
+          request.get(`${base}new`, (err, res, body) => {
+            expect(err).toBeNull();
+            expect(body).toContain("New Topic");
+            done();
+          });
+        });
+    
+    });
+
+    describe("POST /topics/create", () => {
+        const options = {
+            url: `${base}create`,
+            form: {
+                title: "blink-182 songs",
+                description: "What's your favorite blink-182 song?"
+            }
+        };
+  
+        it("should create a new topic and redirect", (done) => {
+            request.post(options,
+                (err, res, body) => {
+                Topic.findOne({where: {title: "blink-182 songs"}})
+                .then((topic) => {
+                    console.log("TOPIC:", topic);
+                    expect(res.statusCode).toBe(303);
+                    expect(topic.title).toBe("blink-182 songs");
+                    expect(topic.description).toBe("What's your favorite blink-182 song?");
+                    done();
+                })
+                .catch((err) => {
+                    console.log(err);
+                    done();
+                });
+                }
+            );
+        });
+    });
+});
---
+++ src/db/queries.topics.js
@@ -0,0 +1,27 @@
+const Topic = require("./models").Topic;
+
+module.exports = {
+
+    getAllTopics(callback){
+        return Topic.all()
+        .then((topics) => {
+        callback(null, topics);
+        })
+        .catch((err) => {
+        callback(err);
+        })
+    },
+
+    addTopic(newTopic, callback){
+        return Topic.create({
+          title: newTopic.title,
+          description: newTopic.description
+        })
+        .then((topic) => {
+          callback(null, topic);
+        })
+        .catch((err) => {
+          callback(err);
+        })
+    }
+}
