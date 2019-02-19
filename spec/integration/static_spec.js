c7cf0572848caaf9958827bd5a8ca890eb6a62d7
Evgheni Curdov <kurdov.e@gmail.com>
2019-02-18 19:51:05 -0800

    view is setup

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -4,9 +4,10 @@ const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
     describe("GET /", () => {
-        it("should return status code 200", (done) => {
+        it("should return status code 200 and have 'Welcome to PostWall' in the body of the response", () => {
         request.get(base, (err, res, body) => {
             expect(res.statusCode).toBe(200);
+            expect(body).toContain("Welcome to PostWall");
             done();
         });
         });
